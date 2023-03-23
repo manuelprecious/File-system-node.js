a2fabe752e34ab669e66bfd705592f1705fde86a
Manuel-Smith <riddicksmith37@gmail.com>
2023-03-23 10:57:49 +0300

    Functionality for adding a file

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const fs = require("node:fs/promises");
+const path = require("node:path");
 
 (async function () {
   try {
+    // commands
+    const CREATE_FILE = "create a file";
+    const DELETE_FILE = "delete a file";
+    const REMAME_FILE = "rename a file";
+    const ADD_TO_FILE = "add to the file";
+
     let commandFilePath = "./command.txt";
     const commandFileHandler = await fs.open(commandFilePath, "r");
 
@@ -18,7 +25,46 @@ const fs = require("node:fs/promises");
 
       // Reading all the content of the file into memory
       await commandFileHandler.read(buffer, offset, contentLength, position);
-      console.log(buffer.toString('utf8'));
+
+      const command = buffer.toString("utf8");
+
+      // Create a file
+
+      // Function for handling file creation.
+      const createFile = async (path) => {
+        try {
+          // Checking whether we already have that file or not.
+          const isFileExisting = await fs.open(path, "r");
+          isFileExisting.close();
+          return console.log(`The file ${path} already exists.`);
+        } catch (err) {
+          const newFileHandle = await fs.open(path, "w");
+          console.log("A new file was successfully created");
+          newFileHandle.close();
+        }
+      };
+
+      // Function for handling file deletion
+      const deleteFile = async (path) => {};
+
+      // Create a file:
+      // Create a file <path>
+      if (command.includes(CREATE_FILE)) {
+        const filePath = path.resolve(
+          command.substring(CREATE_FILE.length + 2)
+        );
+        createFile(filePath);
+      }
+
+    //   // Delete a file
+    //   // delete the file <path>
+    //   if (command.includes(DELETE_FILE)) {
+    //     const filePath = path.resolve(
+    //       command.substring(DELETE_FILE.length + 2)
+    //     );
+
+    //     deleteFile(filePath);
+    //   }
     });
 
     // A command for watching changes in the command.txt file
